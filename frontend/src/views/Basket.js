dfe9cb5f4dfb9f9e0a31362247fd00e3b96999f0
kronseld <velikiygeolog@yandex.ru>
2024-02-14 22:56:27 +0400

    basket

--- frontend/src/views/Basket.js
+++ frontend/src/views/Basket.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import './Basket.css';
 
-const container = document.getElementsByClassName("container");
 let cartItems = JSON.parse(localStorage.getItem("todo items")) || [];
+let clearBasket = function () {
+  localStorage.removeItem('todo items')
+  document.location.reload()
+}
 
 function Basket() {
-for (let i = 0; i < cartItems.length; i++) {
-  container.innerHTML = `<p>${cartItems.header}</p><span>${cartItems.price}</span>`;
-}
+
   
   return (
     <div className="Basket">
         <h1>Корзина</h1>
         <div className='container'>
-        
+          {cartItems.map((item) => (
+            <div className='item'>
+              <p>{item.header}</p>
+              <span>{item.price}$</span>
+            </div>
+          ))}
         </div>
+        <button onClick={clearBasket}>Очистить корзину</button>
+        <button onClick={() => prompt('Укажите номер телефона или почту и мы свяжемся с вами для оформления заказа')}>Оформить заказ</button>
     </div>
   );
 }
